3d5fcabd828371d88d32d0289dba6f0f092ef754
josepholiverr <josepholiverbiz@gmail.com>
2022-06-02 21:29:10 -0700

    update

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by joseph on 6/2/22.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text("This is text from")
+    }
+    
+    
+}
