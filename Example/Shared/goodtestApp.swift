416531ff8d2cef812fbde5648f71860406ff053c
josepholiverr <josepholiverbiz@gmail.com>
2022-06-02 21:31:23 -0700

    update

---
+++ Example/Shared/goodtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  goodtestApp.swift
+//  Shared
+//
+//  Created by joseph on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct goodtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
